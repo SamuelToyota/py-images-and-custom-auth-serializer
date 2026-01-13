7e1b18028463eecf5de1acfa325b89cf859361f5
Samuel Yudi <toyotaa038@gmail.com>
2026-01-13 13:00:34 -0300

    aaaaaa

--- cinema/views.py
+++ cinema/views.py
@@ -1,13 +1,30 @@
-from rest_framework import status, viewsets
+from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from cinema.models import Movie
-from cinema.serializers import MovieImageSerializer
+from cinema.serializers import (
+    MovieSerializer,
+    MovieListSerializer,
+    MovieDetailSerializer,
+    MovieImageSerializer,
+)
 
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return MovieListSerializer
+
+        if self.action == "retrieve":
+            return MovieDetailSerializer
+
+        if self.action == "upload_image":
+            return MovieImageSerializer
+
+        return MovieSerializer
 
     @action(
         methods=["POST"],
@@ -16,10 +33,9 @@ class MovieViewSet(viewsets.ModelViewSet):
     )
     def upload_image(self, request, pk=None):
         movie = self.get_object()
-        serializer = MovieImageSerializer(movie, data=request.data)
+        serializer = self.get_serializer(movie, data=request.data)
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.data, status=status.HTTP_200_OK)
